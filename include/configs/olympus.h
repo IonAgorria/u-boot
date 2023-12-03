5029cdc47e268f30ec2576fd05e86defc740d07e
Svyatoslav Ryhel <clamor95@gmail.com>
2023-12-03 19:34:49 +0200

    board: motorola: olympus: add Atrix 4G MB860 support
    Signed-off-by: Svyatoslav Ryhel <clamor95@gmail.com>

---
+++ include/configs/olympus.h
@@ -0,0 +1,21 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ *  (C) Copyright 2023
+ *  Svyatoslav Ryhel <clamor95@gmail.com>
+ */
+
+#ifndef __CONFIG_H
+#define __CONFIG_H
+
+#include <linux/sizes.h>
+#include "tegra20-common.h"
+
+/* High-level configuration options */
+#define CFG_TEGRA_BOARD_STRING		"Motorola Olympus"
+
+/* Board-specific serial config */
+#define CFG_SYS_NS16550_COM1		NV_PA_APB_UARTB_BASE
+
+#include "tegra-common-post.h"
+
+#endif /* __CONFIG_H */
