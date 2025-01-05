89c32e9d41b996efdf1acfdbf2a7701ce509fccd
Ion Agorria <ion@agorria.com>
2025-01-05 13:49:43 +0100

    ARM: tegra: Use AES engine for crypto functions
    Previously software based AES encryption was used with previously
    known device specific keys (SBK), now that we have AES driver
    we can simply delegate this to the engine without prior knowledge
    of the key (assuming its still loaded)

--- arch/arm/include/asm/arch-tegra/crypto.h
+++ arch/arm/include/asm/arch-tegra/crypto.h
@@ -20,31 +20,23 @@
 int sign_data_block(u8 *source, unsigned int length, u8 *signature);
 
 /**
- * Sign an encrypted block of data
- *
- * \param source	Source data
- * \param length	Size of source data
- * \param signature	Destination address for signature, AES_KEY_LENGTH bytes
- * \param key		AES128 encryption key
- */
-int sign_enc_data_block(u8 *source, unsigned int length, u8 *signature, u8 *key);
-
-/**
  * Encrypt a block of data
  *
  * \param source	Source data
+ * \param dest		Destination data
  * \param length	Size of source data
  * \param key		AES128 encryption key
  */
-int encrypt_data_block(u8 *source, unsigned int length, u8 *key);
+int encrypt_data_block(u8 *source, u8 *dest, unsigned int length);
 
 /**
  * Decrypt a block of data
  *
  * \param source	Source data
+ * \param dest		Destination data
  * \param length	Size of source data
  * \param key		AES128 encryption key
  */
-int decrypt_data_block(u8 *source, unsigned int length, u8 *key);
+int decrypt_data_block(u8 *source, u8 *dest, unsigned int length);
 
 #endif /* #ifndef _CRYPTO_H_ */
